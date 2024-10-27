9bd5296af86ee61d6c6f7419af0eb0355737c6e4
ShunNebula <vyach1369@gmail.com>
2024-10-28 00:29:47 +0700

    # Added new changes to task 3

--- ObjectOrientedPractices/ObjectOrientedPractices/Model/CustomerFactory.cs
+++ ObjectOrientedPractices/ObjectOrientedPractices/Model/CustomerFactory.cs
@@ -12,7 +12,7 @@ namespace ObjectOrientedPractices.Model
         /// <summary>
         /// Список имён
         /// </summary>
-        private static string[] names = File.ReadAllLines(@"C:\Users\User\source\repos\Programming\ObjectOrientedPractices\ObjectOrientedPractices\Services\CustomerNames.txt");
+        private static string[] names = File.ReadAllLines($@"{Environment.CurrentDirectory}\Services\ItemInfos.txt");
 
         /// <summary>
         /// Представляет экземпляр генератора случайных чисел.
